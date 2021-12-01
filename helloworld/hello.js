7ce9776f71e0cc0a04a999f85d7aab2793406f5f
ZhuoranTian <849597043@qq.com>
2021-12-01 16:46:01 +0800

    指向域名

--- helloworld/hello.js
+++ helloworld/hello.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 80
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.sendFile( __dirname + "/public/" + "index.html" );
+  res.sendFile(__dirname + "/public/" + "index.html");
 })
 
 app.listen(port, () => {
