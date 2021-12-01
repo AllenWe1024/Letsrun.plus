1a92a5d94c9c608ca13127add4c9c8ab66b94f5c
ZhuoranTian <849597043@qq.com>
2021-12-01 16:38:28 +0800

    test

--- helloworld/hello.js
+++ helloworld/hello.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile( __dirname + "/public/" + "index.html" );
 })
 
 app.listen(port, () => {
