85af16a242f5d6a22de185aa40a80ce8fe1c39e5
ZhuoranTian <849597043@qq.com>
2021-12-18 22:34:56 +0800

    test

--- helloworld/public/lyyousa/js/lyyousa.js
+++ helloworld/public/lyyousa/js/lyyousa.js
@@ -7,12 +7,26 @@ $(function () {
     single_column_breakpoint: 700
   });
 
-  $("#shengdan").click(function () {
-    // console.log(1);
-    $(".display2").attr('id','gallery-wrapper')
-  })
+  // $("#default").click(function () {
+  //   // console.log(1);
+  //   $(".display1").css("display", "block").siblings(".display2").css("display", "none")
+  //   $(".display1 section").attr('id', 'gallery-wrapper')
+  //   $(".display2 section").removeAttr('id')
 
-  // test
-  $("#test1").click(function () {
-    $(".display2").attr('id','')  })
+  // })
+
+  // $("#shengdan").click(function () {
+  //   // console.log(1);
+  //   $(".display2").css("display", "block").siblings(".display1").css("display", "none")
+  //   $(".display2 section").attr('id', 'gallery-wrapper')
+  //   $(".display1 section").removeAttr('id')
+
+  // })
+
+  // // test
+  // $("#test1").click(function () {
+  //   $(".display2 section").removeAttr('id', '')
+  //   $(".display2").css("display", "none")
+
+  // })
 });
