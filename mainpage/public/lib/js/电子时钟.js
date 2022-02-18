bd9deea160d130a6564331e09bbabc185ceb1f09
ZhuoranTian <849597043@qq.com>
2022-02-18 12:01:17 +0800

    切图

---
+++ mainpage/public/lib/js/电子时钟.js
@@ -0,0 +1,20 @@
+
+var timer = document.querySelector('.timer');
+getTimer()
+setInterval(getTimer, 1000);
+
+function getTimer() {
+    var time = new Date();
+    var year = time.getFullYear();
+    var month = time.getMonth() + 1;
+    var dates = time.getDate();
+    var arr = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+    var day = time.getDay();
+    var h = time.getHours();
+    h = h < 10 ? '0' + h : h;
+    var m = time.getMinutes();
+    m = m < 10 ? '0' + m : m;
+    var s = time.getSeconds();
+    s = s < 10 ? '0' + s : s;
+    timer.innerHTML = '现在时间是：' + year + '年' + month + '月' + dates + '日' + arr[day] + h + ':' + m + ':' + s;
+}
